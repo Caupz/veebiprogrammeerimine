2b9bc6b86414a9738f80fa3cbd516ad64d0d2781
Caupo Helvik <helvikcaupo@users.noreply.github.com>
2018-09-11 14:23:06 +0300

    algfailid

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>IF18 praktikum</title>
+		<link rel="stylesheet" href="http://greeny.cs.tlu.ee/~cauphel/site.css">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet"> 
+	</head>
+
+	<body>
+		<h1>Happy 911</h1>
+		<div class="center">
+			<div class="pictures">
+				<img class="pic" src="happy911.jpg" alt="Happy 911">
+				<img class="pic" src="../../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_2.jpg" alt="Happy 911">
+			</div>
+			<p>Siin on minu <a target="_blank" href="http://tlu.ee">TLÜ</a> õppetöö</p>
+			
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			
+			<div class="btn-container">
+				<a class="btn btn-right" target="_blank" href="../../~robiran/">Paremal Robin teeb ka veebi ></a>
+				<a class="btn btn-left" target="_blank" href="../../~danilat/">< Vasakul Daniil teeb ka veebi</a>
+			</div>
+		</div>
+	</body>
+</html> 
