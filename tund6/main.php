a07ddaa2921728d3329dfc38484be00126dcad43
Caupo Helvik <helvikcaupo@users.noreply.github.com>
2018-10-09 14:19:45 +0300

    tund 6 muudatused

---
+++ tund6/main.php
@@ -0,0 +1,35 @@
+<?php
+require("../tund4/functions.php");
+
+if(isset($_GET['logout'])) {
+    $_SESSION = null;
+    session_destroy();
+}
+
+if(!isset($_SESSION['id'])) {
+    header("Location: index2.php");
+    exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+	<title>Pealeht</title>
+  </head>
+  <body>
+    <h1>Pealeht</h1>
+	<p>See leht on valminud <a href="http://www.tlu.ee" target="_blank">TLÜ</a> õppetöö raames ja ei oma mingisugust, mõtestatud või muul moel väärtuslikku sisu.</p>
+	<hr>
+    <ul>
+        <li><a href="?logout=1">Logi välja</a></li>
+    </ul>
+
+    <p>
+        Olete sisselogitud, <?= $_SESSION['firstname'] ?> <?= $_SESSION['lastname'] ?>.
+    </p>
+    <a href="/logout.php">Logi välja eraldi php faili kaudu</a>
+  </body>
+</html>
